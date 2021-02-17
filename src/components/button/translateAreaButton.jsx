79853b7ddcb9c387a054f4c90582e32eec30b94c
Felipe Kenzo <Kenzo12152@gmail.com>
2021-02-17 15:46:01 -0300

    AreaMovementBtn .scss -> styled-components

--- src/components/button/translateAreaButton.jsx
+++ src/components/button/translateAreaButton.jsx
@@ -3,34 +3,8 @@ import styled from 'styled-components'
 
 import translateArea from './translateArea'
 
-// styled-componennts
-const AreaMovementBtn = styled.button`
-    width: 10rem;
-    padding: 1rem 0rem;
-    text-align: center;
-    font-size: 1.3rem;
-
-    position: absolute;
-    z-index: 10;
-
-    border: none;
-    background: none;
-
-    &:hover {
-        color: white;
-        transition: color .5s ease;
-    }
-
-    top:    ${props => `${props.top    || ''}`};
-    bottom: ${props => `${props.bottom || ''}`};
-    left:   ${props => `${props.left   || ''}`};
-    right:  ${props => `${props.right  || ''}`};
-
-    transform: ${props => `rotate(${props.rotate || 0}deg)`};
-`
-
-export default props => (
-    <AreaMovementBtn 
+const AreaMovementBtn = props => (
+    <Button 
         top={props.top} bottom={props.bottom}
         left={props.left} right={props.right}
         rotate={props.rotate}
@@ -39,5 +13,34 @@ export default props => (
         onMouseEnter={props.onMouseEnter} 
         onMouseLeave={props.onMouseLeave}
 
-    >{props.label}</AreaMovementBtn>
+    >{props.label}</Button>
 )
+
+export default AreaMovementBtn
+
+
+const Button = styled.button`
+    width: 10rem;
+    padding: 1rem 0rem;
+    text-align: center;
+    font-size: 1.3rem;
+    z-index: 10;
+
+    border: none;
+    background: none;
+
+    &:hover {
+        color: white;
+
+        /* variavel: constants.$translate-button-highlight-duration  */
+        transition: color .5s ease;
+    }
+
+    position: absolute;
+    top:    ${props => `${props.top    || ''}`};
+    bottom: ${props => `${props.bottom || ''}`};
+    left:   ${props => `${props.left   || ''}`};
+    right:  ${props => `${props.right  || ''}`};
+
+    transform: ${props => `rotate(${props.rotate || 0}deg)`};
+`
